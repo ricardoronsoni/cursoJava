3e39bdf6f496dbe50393641c8384d134b181ff16
ricardoronsoni <ricardoronsoni@hotmail.com>
2020-07-19 16:31:22 -0300

    criando operacao de instanciacao

--- src/main/java/com/ricardoronsoni/curso/CursoApplication.java
+++ src/main/java/com/ricardoronsoni/curso/CursoApplication.java
@@ -1,13 +1,37 @@
 package com.ricardoronsoni.curso;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursoApplication {
+import com.ricardoronsoni.curso.domain.Categoria;
+import com.ricardoronsoni.curso.repositories.CategoriaRepository;
 
+
+//o commandlinerunner é apenas para inserir os valores no banco antes do rodar a aplicacao
+@SpringBootApplication
+public class CursoApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoApplication.class, args);
+	}
+
+	//esse metodo é apenas para inserir os valores no banco antes do rodar a aplicacao
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "informatica");
+		Categoria cat2 = new Categoria(null, "escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+		
 	}
 
 }
